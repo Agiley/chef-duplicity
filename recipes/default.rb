983b432ff29f79830126b6694f229a049a9a0051
Sebastian <sebastian.johnsson@gmail.com>
2018-03-16 14:05:00 +0100

    Prepare for Chef 14

--- recipes/default.rb
+++ recipes/default.rb
@@ -27,8 +27,8 @@
 package "duplicity"
 package "python-boto"
 
-node.set_unless['duplicity']['database']['user']      =   "root"
-node.set_unless['duplicity']['database']['password']  =   node["mysql"]["server_root_password"]
+node.default_unless['duplicity']['database']['user']      =   "root"
+node.default_unless['duplicity']['database']['password']  =   node["mysql"]["server_root_password"]
 
 # Create archive folder
 directory node["duplicity"]["archive_dir"] do
